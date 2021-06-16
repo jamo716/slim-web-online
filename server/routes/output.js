d674c023b6dd199b7be35b848ce36e996089474d
jamo716 <poligen716@gmail.com>
2021-06-16 14:54:39 -0400

    Using child process instead of execSync.

--- server/routes/output.js
+++ server/routes/output.js
@@ -2,28 +2,76 @@ import express from "express"
 import fs from "fs"
 import csv from "fast-csv"
 import paramSetList from "../data/ParamSetsList.js"
-import {execSync} from "child_process"
+import {execSync, spawn} from "child_process"
 import outputList from "../data/OutputList.js"
 
 
 const router = express.Router()
 
+// router.post("/:id", (req, res) => {
+//   const found = outputList.some((output) => output.id === parseInt(req.params.id))
+//   //the parameter set to compute with SLiM
+//   const paramSetToRun = paramSetList.filter((paramSet) => paramSet.id === parseInt(req.params.id))[0]
+
+//   if(!found){
+//     //stores outputs from csv file for a single simulation run
+//     const fileOutputs = []
+
+//     //synchronous command line execution so data can finish rendering
+//     execSync(`slim -d mutRate=${paramSetToRun.mutRate} -d popSize=${paramSetToRun.popSize} server/slim-scripts/test.slim`, (stdout) => {
+//       //console.log(stdout)
+//     })
+
+//     //reads csv file of outputs then creates an array of JS objects that get sent in JSON format to the front end
+ 
+//       fs.createReadStream("server/slim-output/test.csv")
+//       .pipe(csv.parse({}))
+//       .on("data", (data) => {
+//         const generationOutput = 
+//         {
+//           generation: data[0],
+//           mutCount: data[1]
+//         }
+//         fileOutputs.push(generationOutput)
+//       })
+//       .on("end", () => {
+//         res.status(200).json(fileOutputs)
+//         //format for new output object that goes into the server outputList cache of output objects with an id
+//         const newOutput = {
+//           id: parseInt(req.params.id),
+//           output: fileOutputs
+//         }
+//         //pushing onto server cache of computed outputs
+//         outputList.push(newOutput)
+//       })
+//   }
+//   if(found){
+//     //filter out outputs that were already calculated so that we can return the cached output data
+//     //have to use [0] index because the filter function returns an array of length 1 with the filtered item
+//     const cachedOutput = outputList.filter((item) => item.id === parseInt(req.params.id))[0]
+//     res.status(200).json(cachedOutput.output)
+//   }
+// })
+
 router.post("/:id", (req, res) => {
   const found = outputList.some((output) => output.id === parseInt(req.params.id))
   //the parameter set to compute with SLiM
-  const paramSetToRun = paramSetList.filter((paramSet) => paramSet.id === parseInt(req.params.id))[0]
 
   if(!found){
     //stores outputs from csv file for a single simulation run
     const fileOutputs = []
 
-    //synchronous command line execution so data can finish rendering
-    execSync(`slim -d mutRate=${paramSetToRun.mutRate} -d popSize=${paramSetToRun.popSize} server/slim-scripts/test.slim`, (stdout) => {
-      //console.log(stdout)
-    })
+    const paramSetToRun = paramSetList.filter((paramSet) => paramSet.id === parseInt(req.params.id))[0]
 
-    //reads csv file of outputs then creates an array of JS objects that get sent in JSON format to the front end
- 
+    const child = spawn("slim", ["-d", `mutRate=${paramSetToRun.mutRate}`, "-d", `popSize=${paramSetToRun.popSize}`, "server/slim-scripts/test.slim"])
+
+    child.on("exit", (code, signal) => {
+      if(code){
+          //console.log(code)
+      }
+      if(signal){
+          //console.log(signal)
+      }
       fs.createReadStream("server/slim-output/test.csv")
       .pipe(csv.parse({}))
       .on("data", (data) => {
@@ -44,6 +92,7 @@ router.post("/:id", (req, res) => {
         //pushing onto server cache of computed outputs
         outputList.push(newOutput)
       })
+    })
   }
   if(found){
     //filter out outputs that were already calculated so that we can return the cached output data
