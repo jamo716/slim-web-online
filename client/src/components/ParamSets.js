8904ec21a2f9c2badf8f042958836e664e1b9746
jamo716 <poligen716@gmail.com>
2021-06-16 20:52:30 -0400

    Before I break it with rendering spinner.

--- client/src/components/ParamSets.js
+++ client/src/components/ParamSets.js
@@ -5,6 +5,7 @@ Displays the list of parameter sets stored in app global state.
 
 import ParamSet from "./ParamSet"
 import Paper from "@material-ui/core/Paper"
+import CircularProgress from "@material-ui/core/CircularProgress"
 
 const ParamSets = (({paramSets, onDelete, onRetrieve}) => {
     return(
@@ -12,8 +13,9 @@ const ParamSets = (({paramSets, onDelete, onRetrieve}) => {
         {paramSets.map((paramSet) => {
             return(
                 <Paper key={paramSet.id} elevation={3}>
-                <ParamSet  paramSet={paramSet} onDelete={onDelete} onRetrieve={onRetrieve}/>
+                    <ParamSet  paramSet={paramSet} onDelete={onDelete} onRetrieve={onRetrieve}/>
                 </Paper>
+                
             )
         })}
         </> 
