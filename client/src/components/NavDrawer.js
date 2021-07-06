acc4d99fc4ee61105299e0f4ee81827784a4c4d6
jamo716 <poligen716@gmail.com>
2021-07-06 16:59:14 -0400

    Made drawer swipeable.

--- client/src/components/NavDrawer.js
+++ client/src/components/NavDrawer.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useHistory, useLocation} from "react-router"
 import {makeStyles} from "@material-ui/core"
 import ComputerIcon from '@material-ui/icons/Computer';
 import InfoIcon from '@material-ui/icons/Info';
-import Drawer from "@material-ui/core/Drawer"
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer"
 import Typography from "@material-ui/core/Typography"
 import List from "@material-ui/core/List"
 import ListItem from "@material-ui/core/ListItem"
@@ -14,17 +14,18 @@ import ToolBar from "@material-ui/core/Toolbar"
 import Avatar from "@material-ui/core/Avatar"
 import SlimIcon from "./media/SLiM_256.jpeg"
 import Divider from "@material-ui/core/Divider"
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from "@material-ui/icons/Menu"
 
 const DRAWER_WIDTH = 240
 
 const useStyles = makeStyles((theme) => {
     return {
         appbar: {
-            width: `calc(100% - ${DRAWER_WIDTH}px)`
+            // width: `calc(100% - ${DRAWER_WIDTH}px)`
         },
         drawer: {
             width: DRAWER_WIDTH,
-            
         },
         drawerPaper: {
             width: DRAWER_WIDTH,
@@ -52,6 +53,8 @@ export const NavDrawer = ({children}) => {
     const history = useHistory()
     const location = useLocation()
 
+    const [open, setOpen] = useState(false)
+
     const pages = [
         {
             text: "Neutral Simulation",
@@ -75,6 +78,9 @@ export const NavDrawer = ({children}) => {
             {/* app bar */}
             <AppBar className={classes.appbar} elevation={1}>
                 <ToolBar>
+                    <IconButton edge="start" color="inherit" aria-label="open drawer" onClick={() => setOpen(true)}>
+                        <MenuIcon/>
+                    </IconButton>
                     <Avatar alt="SLiM Icon" src={SlimIcon} variant="square" className={classes.icon}/>
                     <Typography>
                         SLiM Web
@@ -83,24 +89,27 @@ export const NavDrawer = ({children}) => {
             </AppBar>
 
             {/* side navigation drawer */}
-            <Drawer className={classes.drawer} variant="permanent" anchor="left" classes={{paper: classes.drawerPaper}}>
+            <SwipeableDrawer className={classes.drawer} open={open} anchor="left" classes={{paper: classes.drawerPaper}} onClose={() => setOpen(false)} onOpen={() => {}}>
                 <div>
                     <Typography variant="h6" className={classes.title}>
                         Navigation
                     </Typography>
                     <Divider/>
                 </div>
-                {/* list of links to pages on site */}
-                <List>
-                    {pages.map(page => (
-                        <ListItem button key={page.text} onClick={() => history.push(page.path)} className={location.pathName === page.path ? classes.active : null}>
-                            <ListItemIcon>{page.icon}</ListItemIcon>
-                            <ListItemText primary={page.text}/>
-                        </ListItem>
-                    ))}
 
-                </List>
-            </Drawer>
+                {/* list of links to pages on site */}
+                {/* drawer closes on click of any list items */}
+                <div onClick={() => setOpen(false)}>
+                    <List>
+                        {pages.map(page => (
+                            <ListItem button key={page.text} onClick={() => history.push(page.path)} className={location.pathName === page.path ? classes.active : null}>
+                                <ListItemIcon>{page.icon}</ListItemIcon>
+                                <ListItemText primary={page.text}/>
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </SwipeableDrawer>
 
             <div>
                 <div className={classes.toolbar}></div>
