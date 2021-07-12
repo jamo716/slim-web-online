e99e334ea547532bc0314c8838e07626dd3d5357
jamo716 <poligen716@gmail.com>
2021-07-12 15:35:22 -0400

    Can graph multiple outputs in neutral simulation.

--- client/src/neutral-sim/components/OutputChart.js
+++ client/src/neutral-sim/components/OutputChart.js
@@ -3,20 +3,26 @@ import {useContext} from "react"
 import { NeutralSimContext } from '../context/NeutralSimContext'
 import Container from "@material-ui/core/Container"
 import Typography from '@material-ui/core/Typography'
+import Button from "@material-ui/core/Button"
 
 const OutputChart = (() => {
-   
-    const {graphData} = useContext(NeutralSimContext)
+    
+    const {graphData, clearGraphData} = useContext(NeutralSimContext)
 
     return(
         <Container>
+            <Button onClick={() => clearGraphData()}>Clear Data</Button>
             {graphData.length > 0 ? 
                 <div>
                     <Typography variant="h6">{`Output Chart`}</Typography>
                     <LineChart width={500} height={500} data={graphData}>
                         <Tooltip/>
-                        <Line type="monotone" dataKey="mutCount" stroke="#00203FFF" activeDot={{ r: 8 }} />
-                        <XAxis dataKey="generation" height={60}>
+                        {
+                            graphData.map(d => (
+                                <Line dataKey="mutCount" data={d.data} name={d.name} key={d.name} stroke={"#" + Math.floor(Math.random()*16777215).toString(16)}/>
+                            ))
+                        }
+                        <XAxis dataKey="generation" height={60} allowDuplicatedCategory={false}>
                             <Label value="Generation" offset={0} position="insideBottom"/>
                         </XAxis>
                         <YAxis dataKey="mutCount" label={{ value: 'Count of Mutations', angle: -90, position: 'insideLeft' }} height={10}/>
