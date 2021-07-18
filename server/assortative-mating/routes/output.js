13d5aa2e0c0443318ed062c86c41a21820a36f3d
jamo716 <poligen716@gmail.com>
2021-07-18 17:28:19 -0400

    Whitespace.

--- server/assortative-mating/routes/output.js
+++ server/assortative-mating/routes/output.js
@@ -34,58 +34,58 @@ router.get("/:userid/:id", (req, res) => {
 
 //post request new rendered output
 router.post("/:id", (req, res) => {
-    const found = outputs.some((output) => output.id === parseInt(req.params.id))
-  
-    if(!found){
-      //stores outputs from csv file for a single simulation run
-      const fileOutputs = []
-  
-      //first index referenced because the API returns an array of length one
-      const paramsetToRun = paramsets.filter((paramset) => paramset.id === parseInt(req.params.id))[0]
-  
-      const child = spawn("slim", ["-d", `nQTL=${paramsetToRun.nQTL}`, "-d", `popSize=${paramsetToRun.popSize}`, "-d", `assortStr=${paramsetToRun.assortStr}`, "-d", `id=${parseInt(req.params.id)}`, "server/assortative-mating/slim-scripts/assortative-mating.slim"])
-  
-      child.on("exit", (code, signal) => {
-        if(code){
-            //console.log(code)
+  const found = outputs.some((output) => output.id === parseInt(req.params.id))
+
+  if(!found){
+    //stores outputs from csv file for a single simulation run
+    const fileOutputs = []
+
+    //first index referenced because the API returns an array of length one
+    const paramsetToRun = paramsets.filter((paramset) => paramset.id === parseInt(req.params.id))[0]
+
+    const child = spawn("slim", ["-d", `nQTL=${paramsetToRun.nQTL}`, "-d", `popSize=${paramsetToRun.popSize}`, "-d", `assortStr=${paramsetToRun.assortStr}`, "-d", `id=${parseInt(req.params.id)}`, "server/assortative-mating/slim-scripts/assortative-mating.slim"])
+
+    child.on("exit", (code, signal) => {
+      if(code){
+          //console.log(code)
+      }
+      if(signal){
+          //console.log(signal)
+      }
+      fs.createReadStream(`server/assortative-mating/slim-output/${parseInt(req.params.id)}/output.csv`)
+      .pipe(csv.parse({}))
+      .on("data", (data) => {
+        const generationOutput = 
+        {
+          generation: parseInt(data[0]),
+          phenotypes: data.slice(1).map(element => parseInt(element))
         }
-        if(signal){
-            //console.log(signal)
-        }
-        fs.createReadStream(`server/assortative-mating/slim-output/${parseInt(req.params.id)}/output.csv`)
-        .pipe(csv.parse({}))
-        .on("data", (data) => {
-          const generationOutput = 
-          {
-            generation: parseInt(data[0]),
-            phenotypes: data.slice(1).map(element => parseInt(element))
-          }
-          fileOutputs.push(generationOutput)
-        })
-        .on("end", () => {
-          res.status(200).json(fileOutputs)
-          //format for new output object that goes into the server outputList cache of output objects with an id
-          const newOutput = {
-            id: parseInt(req.params.id),
-            userID: parseInt(req.cookies.userID),
-            title: paramsetToRun.title,
-            popSize: paramsetToRun.popSize,
-            assortStr: paramsetToRun.assortStr,
-            nQTL: paramsetToRun.nQTL,
-            output: fileOutputs
-          }
-          //pushing onto server cache of computed outputs
-          outputs.push(newOutput)
-        })
+        fileOutputs.push(generationOutput)
       })
-    }
-    if(found){
-      //filter out outputs that were already calculated so that we can return the cached output data
-      //have to use [0] index because the filter function returns an array of length 1 with the filtered item
-      const cachedOutput = outputs.filter((item) => item.id === parseInt(req.params.id))[0]
-      res.status(200).json(cachedOutput.output)
-    }
-  })
+      .on("end", () => {
+        res.status(200).json(fileOutputs)
+        //format for new output object that goes into the server outputList cache of output objects with an id
+        const newOutput = {
+          id: parseInt(req.params.id),
+          userID: parseInt(req.cookies.userID),
+          title: paramsetToRun.title,
+          popSize: paramsetToRun.popSize,
+          assortStr: paramsetToRun.assortStr,
+          nQTL: paramsetToRun.nQTL,
+          output: fileOutputs
+        }
+        //pushing onto server cache of computed outputs
+        outputs.push(newOutput)
+      })
+    })
+  }
+  if(found){
+    //filter out outputs that were already calculated so that we can return the cached output data
+    //have to use [0] index because the filter function returns an array of length 1 with the filtered item
+    const cachedOutput = outputs.filter((item) => item.id === parseInt(req.params.id))[0]
+    res.status(200).json(cachedOutput.output)
+  }
+})
 
 router.delete("/:userid/:id", (req, res) => {
   try {
